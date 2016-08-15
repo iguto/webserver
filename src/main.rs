e988696fec578b659c4e0ced1f423ae753e0233d
iguto <clc.iguto@gmail.com>
2016-08-15 19:07:53 +0900

    able to join request location and document root

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::io::{Read, Write};
 use std::path::Path;
+use std::fs::File;
+
+const DOCUMENT_ROOT: &'static str = "~/tmp/webserver_document";
 
 #[derive(Debug)]
 pub enum Method {
-    GET
+    GET,
 }
 
 fn main() {
@@ -32,29 +35,52 @@ fn handle_client(mut stream: TcpStream) {
     println!("{:?} : {}", stream, s);
     let request_line = RequestLine::new(&s);
     println!("request line: {:?}", request_line);
+    let doc_root = Path::new(DOCUMENT_ROOT);
 
+
+    let result = request_line.location.starts_with("/");
+    println!("result: {}", result);
+    let subpath = if result {
+        let mut new_path = request_line.location.to_owned();
+        new_path.remove(0);
+        new_path
+    } else {
+        request_line.location
+    };
+    let location = doc_root.join(subpath);
+
+    println!("real location: {:?}", location);
+    if !location.is_file() {
+        println!("location should be a path for file which exists.");
+    } else {
+        let file = File::open(location).expect("could not open file");
+        println!("file: {:?}", file);
+    }
 }
 
 // represents HTTP request line.
 #[derive(Debug)]
-pub struct RequestLine<'a> {
+pub struct RequestLine {
     pub method: Method,
-    pub location: &'a Path
+    pub location: String,
 }
 
-impl<'a> RequestLine<'a> {
-    fn new(raw_request_line: &'a str) -> RequestLine<'a> {
+impl RequestLine {
+    fn new(raw_request_line: &str) -> RequestLine {
         let (method, location) = RequestLine::parse_request_line(&raw_request_line);
         println!("method: {:?}, location: {:?}", method, location);
-        RequestLine { method: method, location: location }
+        RequestLine {
+            method: method,
+            location: location,
+        }
     }
 
-    fn parse_request_line(raw_request_line: &'a str) -> (Method, &'a Path) {
+    fn parse_request_line(raw_request_line: &str) -> (Method, String) {
         let words: Vec<&str> = raw_request_line.split(" ").collect();
-        let method = RequestLine::detect_method(&words[0])
-            .expect("invalid HTTP method");
-       println!("w0 : {}  w1: {}", words[0], words[1]);
-       (method, Path::new(words[1]))
+        let method = RequestLine::detect_method(&words[0]).expect("invalid HTTP method");
+        let location = words[1].trim();
+        println!("w0 : {}  w1: {}", words[0], words[1]);
+        (method, location.to_owned())
     }
 
     fn detect_method(target: &str) -> Option<Method> {
